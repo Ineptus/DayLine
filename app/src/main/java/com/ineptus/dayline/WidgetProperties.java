fb688c399d258bd05bd623a13878b7a89d51fc74
Ineptus <kuba.radzimowski@gmail.com>
2015-03-21 18:36:54 +0100

    Add class to keep widget properties

---
+++ app/src/main/java/com/ineptus/dayline/WidgetProperties.java
@@ -0,0 +1,19 @@
+package com.ineptus.dayline;
+
+
+import java.util.Set;
+
+public class WidgetProperties {
+
+    public int widgetId;
+    public int version;
+
+    public int range;
+    public Set<String> chosenCalendars;
+    public boolean use12hours;
+    public boolean clickable;
+    public boolean labelFreeTime;
+    public boolean mirror;
+    public boolean showAllDay;
+
+}
