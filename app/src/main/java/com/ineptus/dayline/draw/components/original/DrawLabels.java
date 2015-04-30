e7b78dad59a541c6d45db68e591da2960703b057
Ineptus <kuba.radzimowski@gmail.com>
2015-05-01 00:57:20 +0200

    Add white background style

--- app/src/main/java/com/ineptus/dayline/draw/components/original/DrawLabels.java
+++ app/src/main/java/com/ineptus/dayline/draw/components/original/DrawLabels.java
@@ -163,7 +163,7 @@ public class DrawLabels {
 
                 //BACKGROUND
                 bPaint.setColor(label.color);
-                Rect bRect = new Rect((int) (posX-lPaint.measureText(label.text)-5), c.marginTop+label.y-c.labelHalfHeight, posX+5, c.marginTop+label.y+c.labelHalfHeight);
+                Rect bRect = new Rect((int) (posX-lPaint.measureText(label.text)-5), c.marginTop+label.y-c.labelHalfHeight, posX+5, c.marginTop+label.y+c.labelHalfHeight+1);
 
                 c.canvas.drawRect(bRect, bPaint);
                 c.canvas.drawText(text, posX, c.marginTop+label.y+c.textSize*0.33f, lPaint);
@@ -174,6 +174,92 @@ public class DrawLabels {
 
     }
 
+
+    public static void whiteBackground(Contour c) {
+
+        final String freeTimeString = c.context.getResources().getString(R.string.of_free_time);
+
+        //DEFINE PAINT
+        Paint lPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        lPaint.setTextSize(c.textSize);
+        if(c.mirror) {
+            lPaint.setTextAlign(Align.LEFT);
+        } else {
+            lPaint.setTextAlign(Align.RIGHT);
+        }
+        lPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        lPaint.setShadowLayer(2, 1, 1, 0xaa000000);
+
+        //BACKGROUND PAINT
+        Paint bPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        //bPaint.setShadowLayer(1, 1, 1, 0x44000000);
+        bPaint.setColor(0xaaffffff);
+
+        //CANCEL IF TOO NARROW
+        if(lPaint.measureText("asd...") > c.getLabelsWidth()) {
+            return;
+        }
+
+
+        if(c.events.moveToFirst()) {
+            do{
+                if(c.events.current().start < c.start && c.events.current().end > c.end) {
+                    continue;
+                }
+                c.labels.addFromEvent(c.events.current(), 1);
+            } while(c.events.moveToNext());
+        }
+
+        if(c.line.moveToFirst()) {
+            do{
+                if(c.line.current().isFree()) {
+
+                    if(c.line.hasSurrounded(c.line.current()) && Prefs.load(c, Prefs.LABEL_FREE_TIME, true)) {
+
+                        String label;
+
+                        String h = String.valueOf(c.line.current().getDuration()/(1000*60*60)) + " h ";
+                        if (h.equals("0 h ")) h = "";
+                        String min = String.valueOf((c.line.current().getDuration()/(1000*60))%60) +  " m ";
+                        if (min.equals("0 m ")) min = "";
+                        label = h + min + freeTimeString;
+
+
+                        int color = 0xff88BB00;
+                        c.labels.addCustom(label, color, c.getBoxCenterY(c.line.current()), 0);
+                    }
+                }
+            } while (c.line.moveToNext());
+        }
+
+
+        int posX = c.getLabelsPosX();
+
+        if(c.labels.moveToFirst()) {
+            do{
+                Label label = c.labels.current();
+
+                if(label.text == null || label.text == "") {
+                    continue;
+                }
+
+                lPaint.setColor(label.color);
+
+                String text = label.text;
+
+                text = cutText(c, lPaint, text);
+
+                //BACKGROUND
+                Rect bRect = new Rect((int) (posX-lPaint.measureText(label.text)-5), c.marginTop+label.y-c.labelHalfHeight-1, posX+10, c.marginTop+label.y+c.labelHalfHeight);
+
+                c.canvas.drawRect(bRect, bPaint);
+                c.canvas.drawText(text, posX, c.marginTop+label.y+c.textSize*0.33f, lPaint);
+            } while(c.labels.moveToNext());
+        }
+
+
+
+    }
 
 	private static String cutText(Contour c, Paint paint, String text) {
 
